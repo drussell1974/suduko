eacb310eb8319578bc64443d18b5e21951d093dd
Dave <dave@daverussell.co.uk>
2020-02-23 07:35:42 +0000

    remove duplicate code to remove number from cell

--- suduko/board.py
+++ suduko/board.py
@@ -3,6 +3,8 @@ class Board:
         """ set up 9x9 board and results """
         # the starting board
         self.board = board
+        self.steps = 0
+        self.comp = 0
         
         # create results board
         self.result = []
@@ -25,28 +27,30 @@ class Board:
     def removeFromRow(self, x, y, num):
         """ get row array """
         for z in range(0, 9):
-            if num in self.result[x][z] and len(self.result[x][z]) > 1:
-                self.result[x][z].remove(num)
-                self.is_done = False
+            self.removeNumberFromCell(x, z, num)
 
 
     def removeFromCol(self, x, y, num):
         """ get column array """
         for z in range(0, 9):
-            if num in self.result[z][y] and len(self.result[z][y]) > 1:
-                self.result[z][y].remove(num)
-                self.is_done = False
+            self.removeNumberFromCell(z, y, num)  
 
         
     def removeFromSqu(self, x, y, num):
         """ get the square array """
-        x = int(x/3) % 3 * 3
-        y = int(y/3) % 3 * 3
-        for p in range(x,x+3):
-            for q in range(y,y+3):                
-                if num in self.result[p][q] and len(self.result[p][q]) > 1:
-                    self.result[p][q].remove(num)
-                    self.is_done = False
+        x = int(x/3) % 3 * 3 # start at
+        y = int(y/3) % 3 * 3 # start at
+        for x1 in range(x,x+3):
+            for y1 in range(y,y+3):
+                self.removeNumberFromCell(x1, y1, num)
+                
+
+    def removeNumberFromCell(self, x, y, num):
+        """ remove the number from the cell """
+        self.comp = self.comp + 1
+        if num in self.result[x][y] and len(self.result[x][y]) > 1:
+            self.result[x][y].remove(num)
+            self.is_done = False
 
             
     def check(self):
@@ -55,12 +59,12 @@ class Board:
             # iterate all cells
             for x in range(0, 9):
                 for y in range(0, 9):
+                    self.steps = self.steps + 1
                     # check corresponding cells if cell has one number
                     if len(self.result[x][y]) == 1:
                         number = self.result[x][y][0]
-                        # remove number from corressponding cells
+                        # call funtions to remove number from corressponding cells
                         for f in self.remove_func:
-                            # call function
                             f(x, y, number)
             # exit if no changes are made on final pass                            
             if self.is_done == True:
@@ -80,6 +84,7 @@ class Board:
                     number = [0]
                 str_result = str_result + str(number) + " "
             str_result = str_result + "\n"
+        str_result = str_result + "\nStatisics:\n Solved in {} steps, {} comparisons made".format(self.steps, self.comp)
+        str_result = str_result + "\n Comparisons made per step {}".format(self.comp / self.steps)
         return str_result
-        
     
